04faae2d962ab5ca11c7fc59ffd643e4d99b3a07
kvkram2405 <kvkram2405gmail.com>
2020-01-02 16:15:37 +0530

    Website2 Get Data from Db page is added

--- Page_WebPageHome1.go
+++ Page_WebPageHome1.go
@@ -24,6 +24,10 @@ func saveFunc(w http.ResponseWriter, r *http.Request) {
 func delFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>File Has been Deleted </h1>")
 }
+func dataFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Get Data from Db </h1>")
+}
+
 
 func main() {
 	http.HandleFunc("/home", homeFunc)
@@ -32,5 +36,6 @@ func main() {
 	http.HandleFunc("/logout", logoutFunc)
 	http.HandleFunc("/save", saveFunc)
 	http.HandleFunc("/del", delFunc)
+	http.HandleFunc("/data", dataFunc)
 	http.ListenAndServe(":3000", nil)
 }
